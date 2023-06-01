9bb72a428ba44053454ac15151e1b61ac4031906
zidandff <zidandevs@gmail.com>
2023-06-01 21:16:14 +0700

    apply dark mode

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,8 @@
 /** @type {import('tailwindcss').Config} */
+const plugin = require('tailwindcss/plugin')
 module.exports = {
   content: ["./index.html"],
+  darkMode: 'class',
   theme: {
     container: {
       center: true,
@@ -15,6 +17,34 @@ module.exports = {
       },
     },
   },
-  plugins: [],
+  plugins: [
+    plugin(function({ matchUtilities, theme }) {
+      matchUtilities(
+          {
+              'bg-gradient': (angle) => ({
+                  'background-image': `linear-gradient(${angle}, var(--tw-gradient-stops))`,
+              }),
+          },
+          {
+              // values from config and defaults you wish to use most
+              values: Object.assign(
+                  theme('bgGradientDeg', {}), // name of config key. Must be unique
+                  {
+                      10: '10deg', // bg-gradient-10
+                      15: '15deg',
+                      20: '20deg',
+                      25: '25deg',
+                      30: '30deg',
+                      45: '45deg',
+                      60: '60deg',
+                      90: '90deg',
+                      120: '120deg',
+                      135: '135deg',
+                  }
+              )
+          }
+       )
+    })
+  ],
 }
 
